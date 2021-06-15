50e137b0eb6aceb837c10629d04b2a2b3cfa4d2f
aljaztravnik <travnik.aljaz@gmail.com>
2021-06-15 20:21:04 +0200

    added comments

--- main.py
+++ main.py
@@ -3,8 +3,8 @@ from emoji import demojize
 from deep_translator import GoogleTranslator
 
 server = 'irc.chat.twitch.tv'
-nickname = 'crncinimajopravic'
-token = 'oauth:p520kzgbrg6ha4n4wcmhup2v0dtfvg'
+nickname = '' # enter your twitch chat nickname
+token = '' # generate your own twitch token at https://twitchapps.com/tmi/
 channel = '#'
 port = 6667
 
@@ -15,14 +15,14 @@ vticnik.connect((server, port))
 vticnik.send(f"PASS {token}\n".encode('utf-8'))
 vticnik.send(f"NICK {nickname}\n".encode('utf-8'))
 vticnik.send(f"JOIN {channel}\n".encode('utf-8'))
-resp = vticnik.recv(2048).decode('utf-8') #gets rid of the hello message from Twitch
+resp = vticnik.recv(2048).decode('utf-8') # gets rid of the hello message from Twitch
 
 while True:
     resp = vticnik.recv(2048).decode('utf-8')
     if resp.startswith('PING'):
         vticnik.send("PONG\n".encode('utf-8'))
     elif len(resp) > 0:
-        responseList = resp.split(":", 2) #[0] is empty, [1] is the string we have to split to get the message authors username, and [2] is the message
+        responseList = resp.split(":", 2) # [0] is empty, [1] is the string we have to split to get the message authors username, and [2] is the message
         responseUsername = (responseList[1].split("!", 1))[0]
         translatedMsg = GoogleTranslator(source='auto', target='english').translate(text=str(responseList[2]))
         if translatedMsg is not None:
